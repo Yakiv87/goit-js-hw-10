fc63ef8ac9d0c13c13cb057b09b5ba8fc322a783
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-10-03 00:22:44 -0400

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,22 +1,18 @@
 const url = 'https://api.thecatapi.com/v1';
 const api_key = "live_i0tDaOGInqQ26rh7JUwZgUxN2ia9tgGfeQAmLlGg5e4sjZuijJKVoZO121TcrnQ9";
 
-export function fetchBreeds() {
-    return fetch(`${url}/breeds?api_key=${api_key}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        });       
+export async function fetchBreeds() {
+    const response = await fetch(`${url}/breeds?api_key=${api_key}`);
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return await response.json();       
 };
 
-export function fetchCatByBreed(breedId) {
-    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        });  
+export async function fetchCatByBreed(breedId) {
+    const response = await fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`);
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return await response.json();  
 };
