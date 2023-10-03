b38b7b52042a9536b7f5c2e3df7095fc0d827f45
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-10-03 03:01:47 +0000

    Deploying to gh-pages from @ Yakiv87/goit-js-hw-10@9d22a25fe7d5f9c151eae479d7fa0cd1916f5f20 🚀

--- index.67e2ac8f.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((o=>{if(!o.ok)throw new Error("foll");return o.json()})).then((o=>console.log(o))).cath((o=>console.log(o)));
-//# sourceMappingURL=index.67e2ac8f.js.map
--- index.955ee7df.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds").then((function(n){if(!n.ok)throw new Error("foll");return n.json()})).then((function(n){return console.log(n)})).cath((function(n){return console.log(n)}));
-//# sourceMappingURL=index.955ee7df.js.map
